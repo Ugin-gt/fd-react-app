64e6b8ad465e1226e39e981237194acf83d9691a
Ugin-gt <apach575@gmail.com>
2021-03-03 22:16:04 +0200

    Validate UseEffectStopWatch component

--- src/components/UseEffectStopWatch/index.jsx
+++ src/components/UseEffectStopWatch/index.jsx
@@ -1,53 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import styles from './UseEffectStopWatch.module.css';
-import {format} from 'date-fns';
+import { addMilliseconds, format } from 'date-fns';
 
-const StopWatch = props => {
- 
+const UseEffectStopWatch = props => {
   const [time, setTime] = useState(new Date(0, 0, 0, 0, 0, 0, 0));
   const [isRunning, setIsRunning] = useState(false);
 
-  const switchStopWatch = () => {setIsRunning(!isRunning)};
-
-  const tick = (time) => {
-      const newTime = new Date(time.getTime());
-      newTime.setSeconds(newTime.getSeconds() + 1);
-      return { time: newTime };
-    };
-  };
+  const switchStopWatch = () => setIsRunning(!isRunning);
 
   useEffect(() => {
-   
-
-  const clear = () => {
-    clearTimeout(this.timeoutId);
-    this.timeoutId = null;
-  };
+    console.log('useEffect');
+    if (isRunning) {
+      const intervalId = setInterval(() => {
+        setTime(prevTime => addMilliseconds(prevTime, 1000));
+      }, 1000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [isRunning]);
 
   const reset = () => {
-    this.clear();
-    this.setState({ time: new Date(0, 0, 0, 0, 0, 0, 0) });
+    clearInterval();
+    setTime(new Date(0, 0, 0, 0, 0, 0, 0));
   };
-
 
   return (
     <article className={styles.container}>
-    <h1 className={styles.currentTime}>
-      {time.toLocaleTimeString('it-IT')}
-    </h1>
-    <div className={styles.controls}>
-        <button className={styles.btn}
-        onClick={switchStopWatch}> {isRunning? 'STOP':'START'} 
+      <h1 className={styles.currentTime}>{format(time, 'HH:mm:ss')}</h1>
+      <div className={styles.controls}>
+        <button className={styles.btn} onClick={switchStopWatch}>
+          {isRunning ? 'STOP' : 'START'}
         </button>
-        <button className={styles.btn}
-        onClick={reset}>
-        RESET
-      </button>
-    </div>
-  </article>
+        <button className={styles.btn} onClick={reset}>RESET</button>
+      </div>
+    </article>
   );
-    }}
-
-
-
-export default StopWatch;
+};
+export default UseEffectStopWatch;
