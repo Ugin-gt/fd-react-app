62974f7ec73e16493cb80e77ddd37c57855a5b78
Alyona <agerneta@gmail.com>
2021-03-05 17:06:07 +0200

    Add Navigation Menu show

--- src/App.js
+++ src/App.js
@@ -8,36 +8,15 @@ import Calendar from './components/Calendar';
 import FlexContainer from './components/FlexContainer';
 import WindowSize from './components/WindowSize';
 import UserCont from './pages/UserCont';
+import NavigationMenu from './components/NavigationMenu';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 const App = props => {
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/calendar'>Calendar</Link>
-          </li>
-          <li>
-            <Link to='/window'>Window Size</Link>
-          </li>
-          <li>
-            <Link to='/counter'>Counter</Link>
-          </li>
-          <li>
-            <Link to='/user-cont'>UserContext</Link>
-          </li>
-          <li>
-            <Link to='/stop-watch'>UseEffectStopWatch</Link>
-          </li>
-        </ul>
-      </nav>
+      {/* <button onClink={isOpen} > </button> */}
+      <NavigationMenu />
+
       <Switch>
         <Route exact path='/' component={Home} />
 
